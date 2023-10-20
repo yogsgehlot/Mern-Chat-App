4a70f2c2c78e3d8f05c0ca1a21f13fc263e9ba4d
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2023-10-20 12:28:17 +0530

    Update SideDrawer.js

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -170,7 +170,7 @@ function SideDrawer() {
         <DrawerContent>
           <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
           <DrawerBody>
-            <Box d={"flex"} pb={2}>
+            <Box display={"flex"} pb={2}>
               <Input
                 placeholder="Search by name or email"
                 mr={2}
@@ -190,7 +190,7 @@ function SideDrawer() {
                 />
               ))
             )}
-            {loadingChat && <Spinner ml="auto" d="flex" />}
+            {loadingChat && <Spinner ml="auto" display="flex" />}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
