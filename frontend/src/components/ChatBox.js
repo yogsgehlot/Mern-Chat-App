de8d84945528f7b2ac9f5310a58123105b16a8a3
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2023-10-27 00:44:40 +0530

    Update ChatBox.js

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -12,10 +12,11 @@ const Chatbox = ({ fetchAgain, setFetchAgain }) => {
       alignItems={"center"}
       flexDir="column"
       p={3}
-      bg="white"
+      bg="#1f1b1b"
       w={{ base: "100%", md: "68%" }}
-      borderRadius="lg"
-      borderWidth="1px"
+      // borderRadius="lg"
+      // borderWidth="1px"
+      // opacity={"60%"}
     >
       <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
     </Box>
